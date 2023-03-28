f83155920709b8b139e4da8eceaaa60a942d006a
Tanishka <makodetanishka@gmail.com>
2023-03-28 21:16:14 +0530

    Set functionality of prev and next buttons

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -6,9 +6,20 @@ import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousel = () => {
   const [people, setPeople] = useState(list);
+  const [currentPerson, setCurrentPerson] = useState(0);
 
-  const prevSlide = () => {};
-  const nextSlide = () => {};
+  const prevSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+  const nextSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson + 1) % people.length;
+      return result;
+    });
+  };
   return (
     <section className="slider-container">
       {people.map((person, personIndex) => {
@@ -16,7 +27,9 @@ const Carousel = () => {
         return (
           <article
             className="slide"
-            style={{ transform: `translateX(${100 * personIndex}%)` }}
+            style={{
+              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+            }}
             key={id}
           >
             <img src={image} alt={name} className="person-img" />
